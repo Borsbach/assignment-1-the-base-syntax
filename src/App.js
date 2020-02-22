73dab0b03b6485dcf4f3801f403ab0a34a05ee77
Alex Borsbach <sascha.borsbach@msn.com>
2020-02-22 16:35:46 -0600

    updated test 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import UserInput from "./components/UserInput";
+import UserOutput from "./components/UserOutput";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    username: "Alex"
+  };
+
+  usernameChangeHandler = (event) => {
+    this.setState({username: event.target.value})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ol> 
+          <li>Unit 1 Practice Test</li>
+        </ol>
+        <UserInput 
+        onChange={this.usernameChangeHandler}
+        currentName={this.state.username}/>
+        <UserOutput userName={this.state.username}/>
+        <UserOutput userName={this.state.username}/>
+        <UserOutput userName={this.state.username}/>
+      </div>
+    );
+  }
 }
 
 export default App;
